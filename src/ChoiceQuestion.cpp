592ed3bdc0ba79a0eb2dc13f2672db8a0072150a
BitwiseOldOwl <jackvanveld@gmail.com>
2016-04-28 15:35:39 -0400

    Initial Files, VS I

---
+++ src/ChoiceQuestion.cpp
@@ -0,0 +1,69 @@
+/*
+//////////////////////////////////////////////////////////////////////////
+PURPOSE: Implementation file for muiltiple-choice question class
+CREATED: 28 April 2016
+CHANGELOG:
+*		Jacob, 28 April 2016: Created file, implemented class
+//////////////////////////////////////////////////////////////////////////
+*/
+
+#include "../include/ChoiceQuestion.h"
+
+#define ERROR     -777
+
+using namespace std;
+
+MCQuestion::MCQuestion( string init, int numOpts )
+{
+	question = init;
+
+	for ( int k = 0; k < numOpts - 1; ++k )
+	{
+		choiceArr[ k ] = "Placeholder";
+	}
+
+	if ( numOpts != 5 )
+	{
+		for ( int k = 0; k = numOpts; ++k )
+		{
+			choiceArr[ k ] = "Null";
+		}
+	}
+}
+
+void MCQuestion::setAnswer( string init, char ans )
+{
+	int opt = 0;
+	char alphy[ 5 ] = { 'A','B','C','D','E' };
+
+	for ( int k = 0; k < 5; ++k )
+	{
+		if ( ans == alphy[ k ] )
+		{
+			opt = k;
+		}
+	}
+
+	choiceArr[ opt ] = init;
+}
+
+int MCQuestion::convertAnswer( char ans )
+{
+	int converted = 0;
+	char alphy[ 5 ] = { 'A','B','C','D','E' };
+
+	for ( int k = 0; k < 5; ++k )
+	{
+		if ( ans == alphy[ k ] )
+		{
+			converted = k;
+			k = 6;
+		}
+		else
+		{
+			converted = ERROR;
+		}
+	}
+
+	return converted;
+}
